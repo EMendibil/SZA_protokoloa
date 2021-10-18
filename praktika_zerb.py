083b3b14645550b45928bf6038655704cb0403b4
Iñaki Moreno <morenoartabe@gmail.com>
2021-10-18 21:29:03 +0200

    registeUser() function

--- praktika_zerb.py
+++ praktika_zerb.py
@@ -2,33 +2,71 @@
 
 import socket, os, signal, select, random
 
-PORT = 50005
+PORT = 50006
 MAX_BUF = 1024
 MAX_MEZU = 140
 MAX_WAIT = 999
 
-s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
-s.bind( ('', PORT) )
+def registerUser(args):
+    if len(args) != 2:
+        print("parametro desegokia")
+        elkarrizketa.send("Parametro desegokia".encode()) #ER04
+    else:
+        datuak = args[1].split("#")
+        if len(datuak)>3:
+            elkarrizketa.send("Erabiltzaile izenak eta pasahitzak ezin dute # izan".encode()) #ER04
+        elif len(datuak)<3:
+            elkarrizketa.send("Parametroak falta dira".encode()) #ER03
+        else:
+            users = []
+            emails = []
+            open(".users.txt","a")
+            with open(".users.txt","r") as users_file:
+                for line in users_file:
+                    elements=line.split(" ")
+                    users.append(elements[0])
+                    emails.append(elements[2])
+            if datuak[0] not in users and datuak[2] not in emails:
+                f = open(".users.txt","a")
+                f.write(datuak[0]+" "+datuak[1]+" "+datuak[2]+'\n')
+                f.close()
+            elif datuak[0]  in users:
+                print("Erabiltzailea dagoeneko existitzen da") #ER06
+            else:
+                print("Emaila beste erabiltzaile bati dagokio") #ER07
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind(('', PORT))
 
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
 while True:
-	buf, bez_helb = s.recvfrom( MAX_BUF )
-	if not buf:
-		continue
-	
-	if not os.fork():
-		kodea = randInt(10000, 99999)
-		s.close()
-		elkarrizketa = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-		elkarrizketa.connect( bez_helb )
-		while buf:
-			if buf.decode() ==  
-			jasoa, _, _ = select.select( [ elkarrizketa ], [], [], MAX_WAIT )
-			if not jasoa:
-				print( "Itxoite denbora maximoa ({} s.) agortuta. Bezeroa bukatutzat jo da.".format( MAX_WAIT ) )
-				break
-			buf = elkarrizketa.recv( MAX_BUF )
-		elkarrizketa.close()
-		exit( 0 )
+    buf, bez_helb = s.recvfrom(MAX_BUF)
+    if not buf:
+        continue
+
+    if not os.fork():
+        s.close()
+        kodea = random.randint(10000, 99999)
+        print(kodea)
+        elkarrizketa = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        elkarrizketa.connect(bez_helb)
+        while buf:
+            splitBuf = buf.decode().split(" ")
+            print(splitBuf)
+            elkarrizketa.send(buf)
+            if splitBuf[0] == "RG":
+                registerUser(splitBuf)
+            if splitBuf[0] == "ID":
+                elkarrizketa.send(str(kodea).encode())
+            #jasoa, _, _ = select.select([elkarrizketa], [], [], 10)
+            #if not jasoa:
+            #    print("Itxoite denbora maximoa ({} s.) agortuta. Bezeroa bukatutzat jo da.".format(MAX_WAIT))
+            #    break
+            buf = elkarrizketa.recv(MAX_BUF)
+        elkarrizketa.close()
+        exit(0)
 s.close()
