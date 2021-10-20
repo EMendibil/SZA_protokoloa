4ba81795138b410997344b1243f3ee36c72a1225
aparedes1204 <aitorpz1204@gmail.com>
2021-10-21 01:21:15 +0200

    Error control and id inplementation

--- praktika_zerb.py
+++ praktika_zerb.py
@@ -10,16 +10,16 @@ MAX_MEZU = 140
 MAX_WAIT = 10
 
 
-class Timer():
+class Timer:
     def __init__(self):
         self.time = MAX_WAIT
 
     def run(self):
         while True:
-            #print(self.time)
+            # print(self.time)
             self.time -= 1
             sleep(1)
-            if self.time==0:
+            if self.time == 0:
                 break
 
     def display(self):
@@ -28,16 +28,45 @@ class Timer():
     def get_time(self):
         return self.time
 
+def id(args, timer):
+    par = args[1]
+    if len(par.split("#")) != 2:
+        return "ER02"
+    user = par.split("#")[0]
+    password = par.split("#")[1]
+    try:
+        file = open(".users.txt")
+        lines = file.read().split("\n")
+        exist = False
+        for line in lines:
+            if line.split(" ")[0] == user:
+                if line.split(" ")[1] == password:
+                    exist = True
+                break
+
+        if exist:
+            timer_thread = threading.Thread(target=timer.run, daemon=True)
+            timer_thread.start()
+            kodea = random.randint(10000, 99999)
+            msg = f"OK {kodea}#{MAX_WAIT}"
+            return msg
+        else:
+            return "ER08"
+    except FileNotFoundError:
+        return "ER08"
+
+
+
 
 def registeruser(args):
     if len(args) != 2:
-        return "parametro desegokia" #ER04
+        return "ER04" #Parametro desegokia
     else:
         datuak = args[1].split("#")
         if len(datuak)>3:
-            return "Erabiltzaile izenak eta pasahitzak ezin dute # izan" #ER04
+            return "ER04" #Erabiltzaile izenak eta pasahitzak ezin dute # izan
         elif len(datuak)<3:
-            return "Parametroak falta dira" #ER03
+            return "ER03" #Parametroak falta dira
         else:
             users = []
             emails = []
@@ -52,15 +81,20 @@ def registeruser(args):
                 f = open(".users.txt","a")
                 f.write(datuak[0]+" "+datuak[1]+" "+datuak[2]+'\n')
                 f.close()
-            elif datuak[0]  in users:
-                return "Erabiltzailea dagoeneko existitzen da" #ER06
+            elif datuak[0] in users:
+                return "ER06" #Erabiltzailea dagoeneko existitzen da
             else:
-                return "Emaila beste erabiltzaile bati dagokio" #ER07
+                return "ER07" #Emaila beste erabiltzaile bati dagokio
     return "OK"
 
 
 def validatecode(code, sentcode):
-    return "OK" if code == sentcode and code != -1 else "Segurtasun kode okerra" #ER05
+    if code == sentcode:
+        return "OK"
+    elif code == -1:
+        return "Not valid"
+    else:
+        return "ER05" #Segurtasun kode okerra
 
 
 def mezuabidali(info, sender):
@@ -81,13 +115,13 @@ def mezuabidali(info, sender):
                 file.close()
                 return "OK"
             else:
-                return "Erabiltzaile ezezaguna" #ER09
+                return "ER09" #Erabiltzaile ezezaguna
         else:
-            return "Mezu luzeegia" #ER10
+            return "ER10" #Mezuluzeegia
     elif len(info) < 2:
-        return "Hautazko ez den parametro bat falta da" #ER03
+        return "ER03" #Hautazko ez den parametro bat falta da
     else:
-        return "Espero ez zen parametroa. Parametro bat jaso da espero ez zen tokian" #ER02
+        return "ER02" #Espero ez zen parametroa. Parametro bat jaso da espero ez zen tokian
 
 
 def mezuairakurri(user):
@@ -95,16 +129,12 @@ def mezuairakurri(user):
     try:
         file = open(f".{user}.txt")
         mezuak = file.read().split("\n")
-        response.append(f"OK#{len(mezuak)-1}")
-        response = response + mezuak
+        response.append(f"OK {len(mezuak)-1}")
+        response = response + mezuak[:-1]
         return response
     except FileNotFoundError:
-        response.append("OK#0")
+        response.append("OK 0")
         return response
-
-
-def saioaitxi():
-    return -1
 
 
 if __name__ == "__main__":
@@ -123,11 +153,15 @@ if __name__ == "__main__":
         if not os.fork():
             s.close()
             msg = ""
-            kodea = random.randint(10000, 99999)
-
+            usr = ""
             timer = Timer()
-            timer_thread = threading.Thread(target=timer.run, daemon=True)
-            timer_thread.start()
+            kodea = -1
+
+            # kodea = random.randint(10000, 99999)
+            #
+            # timer = Timer()
+            # timer_thread = threading.Thread(target=timer.run, daemon=True)
+            # timer_thread.start()
 
             elkarrizketa = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             elkarrizketa.connect(bez_helb)
@@ -135,32 +169,57 @@ if __name__ == "__main__":
                 splitBuf = buf.decode().split(" ")
                 komandoa = splitBuf[0]
 
-                print(komandoa)
+                # print(komandoa)
 
-                if timer.get_time()==0:
-                    kodea = random.randint(10000, 99999)
+                if timer.get_time() == 0:
 
+                    # kodea = random.randint(10000, 99999)
+                    # timer = Timer()
+                    # timer_thread=threading.Thread(target=timer.run, daemon=True)
+                    # timer_thread.start()
+                    # msg="Kodea expiratu da, berria lortu ID komandoa erabiliz"
+
+                    kodea = -1
                     timer = Timer()
-                    timer_thread=threading.Thread(target=timer.run, daemon=True)
-                    timer_thread.start()
-                    msg="Kodea expiratu da, berria lortu ID komandoa erabiliz"
-                if komandoa == "ID":
-                    msg=str(kodea)
-                elif komandoa == "RG":
-                    msg=registeruser(splitBuf)
 
+                if komandoa == "ID" and kodea != -1:
+                    msg = id(splitBuf, timer)
+                elif komandoa == "RG":
+                    msg = registeruser(splitBuf)
                 elif komandoa in ["MS", "RD", "XT"]:
-                    if validatecode(kodea, splitBuf[1].split('#')[0]) == "OK":
+                    kodestatus = validatecode(kodea, splitBuf[1].split('#')[0])
+                    if kodestatus == "OK":
                         if komandoa == "MS":
                             msg = mezuabidali([splitBuf[1].split('#')[1], splitBuf[1].split('#')[2]], usr)
+                        elif komandoa == "RD":
+                            if len(splitBuf) != 1:
+                                msg = "ER02" #Espero ez zen parametroa. Parametro bat jaso da espero ez zen tokian
+                            else:
+                                msg = mezuairakurri(usr)
+                        elif komandoa == "XT":
+                            kodea = -1
+                            msg = "OK"
+                    elif kodestatus == "Not valid":
+                        continue
+                        #TODO
+                        # Kodea ez da baliozkoa, erantzun bat eman behar da zerbitzaritik?
 
+                    else:
+                        msg = kodestatus
 
                 else:
-                    msg="Komando ezezaguna" #ER01
-                elkarrizketa.send(msg.encode())
+                    msg = "ER01" #Komando ezezaguna
+                if komandoa == "RD":
+                    if len(msg) > 1:
+                        for mezua in msg:
+                            elkarrizketa.send(mezua.encode())
+                    else:
+                        elkarrizketa.send(msg[0].encode())
+                else:
+                    elkarrizketa.send(msg.encode())
                 jasoa, _, _ = select.select([elkarrizketa], [], [], 100)
                 if not jasoa:
-                    print("Itxoite denbora maximoa ({} s.) agortuta. Bezeroa bukatutzat jo da.".format(MAX_WAIT))
+                    print(f"Itxoite denbora maximoa ({MAX_WAIT} s.) agortuta. Bezeroa bukatutzat jo da.")
                     break
                 buf = elkarrizketa.recv(MAX_BUF)
             elkarrizketa.close()
